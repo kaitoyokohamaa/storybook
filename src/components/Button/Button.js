94ac4a62d6a32fb20462a90ace94796063a452d0
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-08-31 21:57:09 +0900

    add btn folder

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Button(props) {
+    const {variant="primary",children,...rest}=props
+    return (
+        <button>
+            
+        </button>
+    )
+}
