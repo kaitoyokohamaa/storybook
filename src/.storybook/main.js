fa8a55b535fba974ce930b939270778cdec3354e
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-01 13:44:06 +0900

    make btn storybook

---
+++ src/.storybook/main.js
@@ -0,0 +1,10 @@
+export const stories = ['../src/**/*.stories.js'];
+export const addons = [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y'
+];
