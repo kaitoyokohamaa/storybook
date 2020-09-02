4302fe939e0364cb61ff79548181b17c355105cf
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-02 12:52:29 +0900

    add knob

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { addDecorator } from '@storybook/react'
 import { ThemeProvider, theme, CSSReset, Box} from '@chakra-ui/core'
-
-addDecorator(story => (
+import {withConsole} from "@storybook/addon-console"
+ addDecorator(story => (
   <ThemeProvider theme={theme}>
     <CSSReset />
     <Box m="4">{story()}</Box> 
   </ThemeProvider>
 ))
+
+addDecorator((storyFn,context)=> withConsole()(storyFn)(context))
